ccb1d6561c3feb32f96f76ea83676ffad6d6732d
JMAC <maclellanj15@gmail.com>
2022-03-18 17:48:08 -0500

    models setup

--- models/index.js
+++ models/index.js
@@ -4,14 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Associations
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'product_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    as: 'product_tag',
+    foreignKey: 'tag_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    as: 'product_tag',
+    foreignKey: 'product_id'
+})
 module.exports = {
   Product,
   Category,
